4616aef4949fbe1d9987766292ca7eeaf2ecc820
Samrat <samratshekhar42@gmail.com>
2017-04-27 22:55:00 +0530

    learning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  
+
+	# simple route
   get 'demo/index'
+  #match "demo/index", :to => "demo#index", :via => :get
+
+
+  # default route
+  #get ':controller(/:action(/:id))'
+
+
+  # root route
+  root 'demo#index'
+  match '/', :to => "demo#index", :via => :get
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
