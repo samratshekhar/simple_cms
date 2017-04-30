fee1e863e1c9df7fb0743fc913aa077321e553be
Samrat <samratshekhar42@gmail.com>
2017-04-30 22:10:05 +0530

    continue routes

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,4 +4,22 @@ class DemoController < ApplicationController
   
   def index
   end
+
+  def hello
+
+  	render('hello')
+  	#render('demo/hello')
+  	#render(:match => "demo/hello")
+  	
+  end
+
+  def other_hello
+
+  	#redirect_to(:action => 'hello')
+  	redirect_to(:controller => 'demo', :action => 'hello')
+  	#  redirect_to('http://google.com')
+  	
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,19 @@ Rails.application.routes.draw do
   
 
 	# simple route
-  get 'demo/index'
-  #match "demo/index", :to => "demo#index", :via => :get
 
+  # match "demo/index", :to => "demo#index", :via => :get
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
 
   # default route
-  #get ':controller(/:action(/:id))'
+  # get ':controller(/:action(/:id))'
 
 
   # root route
   root 'demo#index'
-  match '/', :to => "demo#index", :via => :get
+  # match '/', :to => "demo#index", :via => :get
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
