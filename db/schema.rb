e09acab1d838ccc52070ca13bafbe06365e38cf0
Samrat <samratshekhar42@gmail.com>
2017-05-01 23:27:02 +0530

    perform first migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170501173127) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "first_name", limit: 40
+    t.string   "last_name",  limit: 80
+    t.string   "email",                 default: "", null: false
+    t.string   "password",   limit: 80
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
 
 end
