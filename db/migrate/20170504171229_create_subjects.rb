201e95b6fa5d98b6fc40885f936f319a6d1e203e
Samrat <samratshekhar42@gmail.com>
2017-05-04 23:02:24 +0530

    add subject model

---
+++ db/migrate/20170504171229_create_subjects.rb
@@ -0,0 +1,16 @@
+class CreateSubjects < ActiveRecord::Migration[5.0]
+  def up
+    create_table :subjects do |t|
+
+    	t.string "name", :limit => 100
+    	t.integer "position"
+    	t.boolean "visible", :default => true
+
+      t.timestamps
+    end
+  end
+
+  def down
+  	drop_table :subjects
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501180828) do
+ActiveRecord::Schema.define(version: 20170504171229) do
 
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "first_name",      limit: 40
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170501180828) do
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
     t.index ["username"], name: "index_admin_users_on_username", using: :btree
+  end
+
+  create_table "subjects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "name",       limit: 100
+    t.integer  "position"
+    t.boolean  "visible",                default: true
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
   end
 
 end
